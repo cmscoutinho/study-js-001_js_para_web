92ba45c81df9fa829eb8eab93a829e10507d48be
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-08-25 11:42:13 -0300

    Renamed directories with wrong name (mostly 04 -> 03).

---
+++ exemp_03_05/main.js
@@ -0,0 +1,7 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
