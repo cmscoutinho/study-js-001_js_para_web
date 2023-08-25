2f4adedaf62d6e13a368ba74f199317fe82276e8
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-08-25 11:37:58 -0300

    Added loop to iterate through "listaDeTeclas"

--- exemp_04_05/main.js
+++ exemp_04_05/main.js
@@ -5,3 +5,11 @@ function tocaSomPom () {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+}
