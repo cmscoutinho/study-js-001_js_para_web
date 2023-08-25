636926f8ee04f6b9a743bba18568110431c4d17a
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-08-25 11:56:06 -0300

    Added example 04-06. Introduced anonymous functions.

---
+++ exemp_04_06/main.js
@@ -0,0 +1,15 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom('#som_tecla_pom');
+    };
+    contador = contador + 1;
+    console.log(contador);
+}
