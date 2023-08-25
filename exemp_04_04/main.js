3da615660a8d9402921d19ecc541b90601f3ff07
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-08-25 11:50:45 -0300

    Created example 04-04. Made 'tocaSom' function more general.

---
+++ exemp_04_04/main.js
@@ -0,0 +1,15 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador = contador + 1;
+    console.log(contador);
+}
