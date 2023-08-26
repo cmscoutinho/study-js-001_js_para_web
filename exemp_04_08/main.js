4f941eddc79c1c95cf6354f1c35e9630800dbc4d
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-08-26 12:08:24 -0300

    Added example 04-08. Introduced 'for' loop.

---
+++ exemp_04_08/main.js
@@ -0,0 +1,16 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // template string
+
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom(idAudio);
+    };
+
+}
