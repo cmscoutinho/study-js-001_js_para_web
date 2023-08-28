24900dd17faf86c25788caaf5a4a89c3d8ef418e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-08-28 12:27:35 -0300

    Added example 05_07. Changed 'tocaSom' function to check for not null and 'audio' elemnts before playing.

---
+++ exemp_05_07/main.js
@@ -0,0 +1,32 @@
+function tocaSom (seletorAudio) {
+    //document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null) {
+        if (elemento.localName === 'audio')
+            elemento.play();
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // template string
+
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function (evento) { 
+        if (evento.code === 'Space' || evento.code === 'Enter') { 
+            tecla.classList.add('ativa');
+       }
+    }
+
+    tecla.onkeyup = function () { 
+        tecla.classList.remove('ativa');
+    }
+
+}
